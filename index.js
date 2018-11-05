aa0a2d13f95e526d39bd4d9b25da7ae34247eb9c
Jflinchum <jonflinchum@gmail.com>
2018-11-05 10:55:39 -0500

    Added add and post functions to bot which saves files and posts them based off of trigger words given by the user. Edited git ignore to ignore reactions folder and .DS_Store mac files.

--- index.js
+++ index.js
@@ -1,30 +1,97 @@
 'use strict';
 
-const discord = require('discord.js');
-const token = process.env.DISCORD_TOKEN;
-console.log(token);
+const { Client, Attachment } = require('discord.js');
+const fs = require('fs');
+const request = require('request');
+const mkdirp = require('mkdirp');
 
-const bot = new discord.Client();
+const PATH = './reactions';
+const TOKEN = process.env.DISCORD_TOKEN;
+
+const download = (url, fileName, extension, cb) => {
+  const fullPath = `${PATH}/${fileName}.${extension}`;
+  mkdirp.sync(PATH);
+  const files = fs.readdirSync(PATH);
+  for (let i = 0; i < files.length; i++) {
+    if (files[i].includes(fileName)) {
+      return cb('File name already exists');
+    }
+  }
+  request.head(url, (err, res, body) => {
+    if (err) {
+      return cb(err);
+    }
+    request(url).pipe(fs.createWriteStream(fullPath)).on('close', cb);
+  });
+};
+
+const bot = new Client();
 
 bot.on('ready', () => {
   console.log('Logged in');
 });
 
+
 bot.on('message', message => {
+  // Check to make sure the message is a command
   if (message.content[0] !== '!') {
     return;
   }
+  // Split the command by spaces
   let cmd = message.content.split(' ');
   console.log(message);
   console.log(cmd);
+
+  // Get any attachments associated with message
   const attach = message.attachments.array();
+
+  // Add
   if (cmd[0] === '!add') {
+    let fileName, url, exten;
     if (attach.length > 0) {
-      console.log(attach);
+      // Handling attachment images
+      fileName = cmd[1];
+      url = attach[0].url;
     } else {
-      console.log(cmd[1]);
+      // If the image is contained in url
+      fileName = cmd[2];
+      url = cmd[1];
     }
+    // The extension of the file
+    exten = url.substr((url.lastIndexOf('.') + 1));
+    if (!fileName) {
+      message.channel.send('Please specify a name.');
+      return;
+    } else if (!exten) {
+      message.channel.send('Could not find file extension');
+    }
+    download(url, fileName, exten, (err) => {
+      if (err) {
+        message.channel.send(err);
+      } else {
+        message.channel.send('Successfully added!');
+      }
+    });
+  } else if (cmd[0] === '!post') {
+    const files = fs.readdirSync(PATH);
+    const name = cmd[1];
+    let attach;
+    // Find the file associated with the name
+    for (let i = 0; i < files.length; i++) {
+      if (files[i].includes(name)) {
+        attach = new Attachment(`${PATH}/${files[i]}`);
+      }
+    }
+    if (!attach) {
+      message.channel.send('Could not find file.');
+      return;
+    }
+    // Send the attachment
+    message.channel.send(attach)
+      .catch(() => {
+        message.channel.send('Could not find file.');
+      });
   }
 });
 
-bot.login(token);
+bot.login(TOKEN);
