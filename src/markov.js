f43056ff152bf55f975c02e80c37a18eaa97e493
SPCoffey <sean@coffeyhouse.com>
2018-11-09 18:51:29 -0500

    Refactor of markov to allow searching up to 1000 messages back

--- src/markov.js
+++ src/markov.js
@@ -17,26 +17,60 @@ var intros = [
 
 const markov = (user, guild, origChannel) => {
   markovString = '';
+  channelsDone = 0;
+  channelLength = 0;
+
   guild.channels.tap(channel => {
     if (channel.type === 'text') {
       channelLength++;
       channel.fetchMessages({ limit: 100 })
-        .then(messages => asyncMarkov(
-          user,
-          messages.filter(m => m.author.id === user.id),
-          origChannel))
-        .catch(console.error);
+        .then(messages => {
+          asyncMarkov(
+            user,
+            messages.filter(m => m.author.id === user.id),
+            origChannel);
+          if (messages.size >= 100) {
+            markovRecursive(messages.last(), user, channel, origChannel, 10);
+          } else {
+            channelsDone++;
+            if (channelsDone === channelLength) {
+              postMarkov(user, origChannel);
+            }
+          }
+        }).catch(console.error);
     }
   });
 };
 
+const markovRecursive = (message, user, channel, origChannel, loop) => {
+  if (loop <= 0) {
+    channelsDone++;
+    if (channelsDone === channelLength) {
+      postMarkov(user, origChannel);
+    }
+    return;
+  }
+
+  channel.fetchMessages({ limit: 100, before: message.id })
+    .then(messages => {
+      asyncMarkov(
+        user,
+        messages.filter(m => m.author.id === user.id),
+        origChannel);
+      if (messages.size >= 100) {
+        markovRecursive(messages.last(), user, channel,
+          origChannel, loop - 1);
+      } else {
+        channelsDone++;
+        if (channelsDone === channelLength) {
+          postMarkov(user, origChannel);
+        }
+      }
+    }).catch(console.error);
+};
+
 const asyncMarkov = (user, messages, origChannel) => {
   messages.tap((message) => { markovString += (message.content + '\n'); });
-
-  channelsDone++;
-  if (channelsDone === channelLength) {
-    postMarkov(user, origChannel);
-  }
 };
 
 const postMarkov = (user, origChannel) => {
