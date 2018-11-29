28a4951fbfc9afa63ca4afd53a888a8402d2cf92
Jflinchum <jonflinchum@gmail.com>
2018-11-29 17:06:25 -0500

    Added pagination to help and list commands.

--- src/help.js
+++ src/help.js
@@ -1,19 +1,21 @@
 'use strict';
 const fs = require('fs');
 const path = require('path');
+const { sendTextBlock } = require('./util');
 
 /**
  * Posts the contents of the help file to the channel
  *
  * @param {Object} message - The Discord Message Object that initiated
  * the command
+ * @param {Integer} page - The page number
  */
-const help = (message) => {
+const help = (message, page) => {
   message.delete();
   const helpText = fs.readFileSync(
-    `${path.dirname(require.main.filename)}/help.txt`
+    `${path.dirname(require.main.filename)}/help.txt`, 'utf8'
   );
-  message.channel.send(`\`\`\`${helpText}\`\`\``);
+  sendTextBlock({text: helpText, message, page});
 };
 
 module.exports = {
