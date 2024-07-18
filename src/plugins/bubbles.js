4fd8ed1d5d063c3f7c29389eff68305df36e4f59
Jflinchum <jonflinchum@gmail.com>
2024-07-18 17:12:00 -0400

    minor fixes to bubbles command

--- src/plugins/bubbles.js
+++ src/plugins/bubbles.js
@@ -2,18 +2,12 @@
 const { ApplicationCommandType, ApplicationCommandOptionType } = require('discord.js');
 const BUBBLES_USAGE = '`usage: !bubbles [<width> [<height>]]`';
 const POP_TEXT = '||pop||';
-const DEFAULT_SIZE = [5, 10];
+const DEFAULT_WIDTH = 5;
+const DEFAULT_HEIGHT = 10
 const { setReplayButton, isDiscordCommand, getReplyFunction } = require('./util');
 
 const generateBubbleWrap = (width, height, message) => {
   let doReply = getReplyFunction(message);
-
-  if (isNaN(width)) {
-	width = DEFAULT_SIZE;
-  }
-  if (isNaN(height)) {
-	height = DEFAULT_SIZE;
-  }
 
   if (width < 1) {
     doReply(BUBBLES_USAGE);
@@ -24,7 +18,13 @@ const generateBubbleWrap = (width, height, message) => {
     return;
   }
 
-  let finalMessage = Array(height).fill(Array(width).fill(POP_TEXT));
+  let finalMessage = Array(height).fill(Array(width).fill(POP_TEXT).join('')).join('\n');
+
+  if (finalMessage.length > 2000) {
+    doReply('You\'re bubbling too hard! Keep the bubblewrap a bit smaller');
+    return;
+  }
+
   doReply(finalMessage).then((bubbleMessage) => {
     if (!bubbleMessage && isDiscordCommand(message)) {
       message.fetchReply().then((bubbleMessage) => {
@@ -32,7 +32,7 @@ const generateBubbleWrap = (width, height, message) => {
           const reactionUser = reaction.users.cache.last();
           const memberObject = message.guild.members.cache.get(reactionUser.id);
           message.member = memberObject;
-          generateBubbleWrap(amount, sides, message);
+          generateBubbleWrap(width, height, message);
         });
       });
     } else {
@@ -40,16 +40,18 @@ const generateBubbleWrap = (width, height, message) => {
         const reactionUser = reaction.users.cache.last();
         const memberObject = message.guild.members.cache.get(reactionUser.id);
         message.member = memberObject;
-        generateBubbleWrap(amount, sides, message);
+        generateBubbleWrap(width, height, message);
       });
     }
+  }).catch((err) => {
+    console.log(err);
   });
 };
 
 const handleDiscordCommand = (interaction) => {
   if (interaction.commandName === 'bubbles') {
-    const width = interaction.options[0].value;
-    const height = interaction.options[1].value;
+    const width = interaction.options.get('width')?.value || DEFAULT_WIDTH;
+    const height = interaction.options.get('height')?.value || DEFAULT_HEIGHT;
     generateBubbleWrap(parseInt(width, 10), parseInt(height), interaction);
   }
 };
